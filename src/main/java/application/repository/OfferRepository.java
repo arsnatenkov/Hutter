95a50e2392d16512773f424c675dec566121c9ef
t0pcup <60294628+t0pcup@users.noreply.github.com>
2021-04-16 00:12:30 +0300

    offer class added and preparing for new tables in db (list of offers soon) :rat:

---
+++ src/main/java/application/repository/OfferRepository.java
@@ -0,0 +1,8 @@
+//package application.repository;
+//
+//import application.entity.Offer;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface OfferRepository extends JpaRepository<Offer, Long> {
+////    TODO
+//}
